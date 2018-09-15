b0ba3d4e8b659550db3155a6e8261361a1cbca46
Jake <jakedenison16@gmail.com>
2018-09-14 20:29:03 -0400

    Slight refactor

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def sieve():
     p = 2       # constant since 2 is the smallest prime number
     for i in range(n+1):        # create an array with an index of n+1
         primes.append(True)
-    while p * p <= n:
+    while p * p <= n:       # if p^2 is outside our set, then we know we have already reached the largest prime in the set
         if primes[p] == True:
-            for i in range(p * 2, n+1, p):
+            for i in range(p * p, n+1, p):      
                  primes[i] = False
         p += 1
     for i in range(2, n+1):
